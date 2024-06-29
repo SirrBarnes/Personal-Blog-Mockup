11cebbde9d7eaf068dbeb9acfd185ec05a31bd63
SirrBarnes <torressergio1357@gmail.com>
2024-06-29 15:35:21 -0400

    6/29/24 code changes

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,11 +1,24 @@
 // TODO: Create a variable that selects the main element, and a variable that selects the back button element
+const mainEl = document.querySelector("main");
+const backButt = document.querySelector("#back")
 
 // TODO: Create a function that builds an element and appends it to the DOM
+function buildElement() {
+    
+}
 
 // TODO: Create a function that handles the case where there are no blog posts to display
+function noBlogPost() {
+
+}
 
 // TODO: Create a function called `renderBlogList` that renders the list of blog posts if they exist. If not, call the no posts function.
+function renderBlogList() {
+
+}
 
 // TODO: Call the `renderBlogList` function
+renderBlogList();
 
 // TODO: Redirect to the home page using the `redirectPage` function found in logic.js when the back button is clicked
+backButt.addEventListener("click", redirectPage);
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,14 +2,18 @@
 
 
 // TODO: Create a function that reads from local storage and returns the data. If no data exists, return an empty array.
+function readLocalStorage (){
 
+}
 
 // TODO: Create a function called `storeLocalStorage` that takes a given object and saves the new data to the existing blog data in local storage.
-
+function storeLocalStorage() {
+    
+}
 
 // ! Use the following function whenever you need to redirect to a different page
 
-let redirectURL = '';
+let redirectURL = 'index.html';
 
 const redirectPage = function (url) {
   redirectURL = url;
